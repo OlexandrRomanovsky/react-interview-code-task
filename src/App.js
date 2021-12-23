fbbb45c1406924b9db4546582039dd0cb0233b0b
OlexandrRomanovsky <olexa.romanowsky@gmail.com>
2021-12-23 18:38:48 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import './style.css';
 
 export default function App() {
   const [num1, setNum1] = useState(0);
-
   const [num2, setNum2] = useState(0);
 
   function onClick1() {
     setTimeout(() => setNum1(num1 + 1), 1000);
   }
-
   function onClick2() {
     setTimeout(() => setNum2(num2 + 1), 1000);
   }
@@ -19,26 +17,19 @@ export default function App() {
   return (
     <div>
       <h1>Hello!</h1>
-
       <h3>{num1}</h3>
-
       <button onClick={onClick1}>Click</button>
-
       <Child number={num2} callback={onClick2} />
     </div>
   );
 }
 
 function Child({ number, callback }) {
-  console.log(number, 'Child');
 
   return (
     <div style={{ backgroundColor: getRandomColor() }}>
-
       <h3>{number}</h3>
-
       <button onClick={callback}>Click</button>
-
     </div>
   );
 }
